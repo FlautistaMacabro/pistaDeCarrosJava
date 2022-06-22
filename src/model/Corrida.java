067ebcafb0c675d029f69d33abbb84600637786b
Vinícius Pelegrineli <vinipelebom2@gmail.com>
2022-06-21 21:39:16 -0300

    Consertando cálculo de voltas

--- src/model/Corrida.java
+++ src/model/Corrida.java
@@ -74,7 +74,7 @@ public class Corrida {
           voltaRegistro = registrosVoltas.getListaVoltasClone().get(voltaAtual-2);
         }
       }
-      voltaRegistro.addListaEventosGerais("O "+carro.getNome()+" COMPLETOU A "+voltasPercorridasCarro+"º VOLTA!\n");
+      voltaRegistro.addListaEventosGerais("O "+carro.getNome()+" COMPLETOU A "+(voltasPercorridasCarro-1)+"º VOLTA!\n");
 //  Mensagem de VOLTA REALIZADA
 //      System.out.println("O "+nomeCarro+" acabou de COMPLETAR UMA VOLTA! Restam "+(getRegistrosVoltas().getQuantVoltas()-carro.getVoltasPercorridas()));
       return true;
